27e76b387875251bd09837745818ccd04fd2c433
weifp <jadic@163.com>
2014-08-07 18:06:27 +0800

    update sql

--- doc/st.sql
+++ doc/st.sql
@@ -10,10 +10,34 @@ Target Server Type    : MYSQL
 Target Server Version : 50525
 File Encoding         : 65001
 
-Date: 2014-07-25 18:03:09
+Date: 2014-08-07 18:05:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tab_charge_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `tab_charge_detail`;
+CREATE TABLE `tab_charge_detail` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `CityCardNo` varchar(30) DEFAULT '' COMMENT '市民卡号',
+  `CardType` tinyint(255) DEFAULT NULL COMMENT '卡片类型',
+  `BankCardNo` varchar(30) DEFAULT '' COMMENT '充值时采用的银行卡卡号或者充值卡卡号',
+  `ChargeTime` datetime DEFAULT NULL COMMENT '充值时间',
+  `ChargeType` tinyint(255) DEFAULT '0' COMMENT '充值类型  0：现金  1：银联卡  3：充值卡',
+  `ChargeAmount` int(255) DEFAULT NULL COMMENT '充值金额，单位：分',
+  `BalanceBeforeCharge` int(255) DEFAULT NULL COMMENT '充值前余额',
+  `TAC` varchar(20) DEFAULT '' COMMENT '圈存过程中产生的tac',
+  `ASN` varchar(20) DEFAULT '' COMMENT 'IC卡应用序列号',
+  `TSN` varchar(20) DEFAULT '' COMMENT 'IC卡交易序列号',
+  `ChargeSNo` int(11) DEFAULT NULL COMMENT '充值交易流水号（终端维护)',
+  `TerminalID` bigint(20) DEFAULT NULL COMMENT '充值所在终端',
+  `POSID` varchar(20) DEFAULT '' COMMENT 'pos编号',
+  `SAMID` varchar(20) DEFAULT '' COMMENT 'sam编号',
+  `AgencyNo` varchar(20) DEFAULT '' COMMENT '充值点编号',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tab_module
@@ -26,10 +50,18 @@ CREATE TABLE `tab_module` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of tab_module
+-- Table structure for tab_refund
 -- ----------------------------
-INSERT INTO `tab_module` VALUES ('1', 'a');
-INSERT INTO `tab_module` VALUES ('2', 'b');
+DROP TABLE IF EXISTS `tab_refund`;
+CREATE TABLE `tab_refund` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CardNo` varchar(30) DEFAULT NULL COMMENT '卡号',
+  `Amount` int(255) DEFAULT NULL COMMENT '退款金额',
+  `Type` tinyint(255) DEFAULT NULL COMMENT '充值类型  0：现金  1：银行卡',
+  `Status` tinyint(255) DEFAULT NULL COMMENT '状态  0：新建未退回  1：退回',
+  `InsertTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tab_terminal
@@ -37,18 +69,12 @@ INSERT INTO `tab_module` VALUES ('2', 'b');
 DROP TABLE IF EXISTS `tab_terminal`;
 CREATE TABLE `tab_terminal` (
   `id` bigint(19) NOT NULL COMMENT '终端ID  12位',
-  `typeId` smallint(6) DEFAULT NULL COMMENT '终端类型，默认0',
+  `typeId` smallint(6) DEFAULT '0' COMMENT '终端类型，默认0',
   `position` varchar(255) DEFAULT NULL COMMENT '终端投放地址',
   `enabled` tinyint(2) DEFAULT '0' COMMENT '启用状态 0：启用 1：停用',
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tab_terminal
--- ----------------------------
-INSERT INTO `tab_terminal` VALUES ('123456', '2', 'a', '0', 'sfd');
-INSERT INTO `tab_terminal` VALUES ('112233445566', '1', 'abcd', '0', 'aaa');
 
 -- ----------------------------
 -- Table structure for tab_terminal_module_status
@@ -61,12 +87,6 @@ CREATE TABLE `tab_terminal_module_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of tab_terminal_module_status
--- ----------------------------
-INSERT INTO `tab_terminal_module_status` VALUES ('12345678', '1', '1');
-INSERT INTO `tab_terminal_module_status` VALUES ('12345678', '2', '1');
-
--- ----------------------------
 -- Table structure for tab_terminal_status
 -- ----------------------------
 DROP TABLE IF EXISTS `tab_terminal_status`;
@@ -74,21 +94,16 @@ CREATE TABLE `tab_terminal_status` (
   `TERMINALID` bigint(19) NOT NULL COMMENT '终端ID  主键',
   `OnlineStatus` tinyint(2) DEFAULT NULL COMMENT '设备在线状态  0：未知  1：在线  2：离线',
   `LastOnlineTime` datetime DEFAULT NULL COMMENT '最近一次的在线时间',
-  `M1STATUS` tinyint(2) DEFAULT NULL COMMENT '市民卡读写模块 (0:未知 1:正常 2:故障   下同)',
-  `M2STATUS` tinyint(2) DEFAULT NULL COMMENT '现金模块',
-  `M3STATUS` tinyint(2) DEFAULT NULL COMMENT '银联模块',
-  `M4STATUS` tinyint(2) DEFAULT NULL COMMENT '打印模块',
-  `M5STATUS` tinyint(2) DEFAULT NULL COMMENT '身份证读取模块',
-  `M6STATUS` tinyint(2) DEFAULT NULL COMMENT '密码键盘模块',
-  `M7STATUS` tinyint(2) DEFAULT NULL,
-  `M8STATUS` tinyint(2) DEFAULT NULL,
+  `M1STATUS` tinyint(2) DEFAULT '0' COMMENT '市民卡读写模块 (0:未知 1:正常 2:故障   下同)',
+  `M2STATUS` tinyint(2) DEFAULT '0' COMMENT '现金模块',
+  `M3STATUS` tinyint(2) DEFAULT '0' COMMENT '银联模块',
+  `M4STATUS` tinyint(2) DEFAULT '0' COMMENT '打印模块',
+  `M5STATUS` tinyint(2) DEFAULT '0' COMMENT '身份证读取模块',
+  `M6STATUS` tinyint(2) DEFAULT '0' COMMENT '密码键盘模块',
+  `M7STATUS` tinyint(2) DEFAULT '0',
+  `M8STATUS` tinyint(2) DEFAULT '0',
   PRIMARY KEY (`TERMINALID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tab_terminal_status
--- ----------------------------
-INSERT INTO `tab_terminal_status` VALUES ('1122334456', '1', '2014-07-18 15:27:32', '1', '2', '3', '3', '1', '2', '1', '0');
 
 -- ----------------------------
 -- Table structure for tab_terminal_type
@@ -101,10 +116,6 @@ CREATE TABLE `tab_terminal_type` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='终端类型';
 
 -- ----------------------------
--- Records of tab_terminal_type
--- ----------------------------
-
--- ----------------------------
 -- Table structure for tab_users
 -- ----------------------------
 DROP TABLE IF EXISTS `tab_users`;
@@ -115,8 +126,3 @@ CREATE TABLE `tab_users` (
   `memo` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tab_users
--- ----------------------------
-INSERT INTO `tab_users` VALUES ('1', 'admin', 'admin', 'admin');
